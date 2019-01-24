8f623b96fc2655a1c053b105107d354474947c26
Alex <13521233954@163.com>
2019-01-24 10:25:33 +0800

    create pro

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QFile styleFile("normal.qss");
+    if (styleFile.open(QFile::ReadOnly))
+    {
+        app.setStyleSheet(styleFile.readAll());
+        styleFile.close();
+    }
+
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
